2de2415f4adc4ba0fbdc5d4f3759219750e7e29f
Rakshit Sakhuja <rakshit.sakhuja345@gmail.com>
2026-04-21 17:17:37 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -4,7 +4,22 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
+
+type RespValue struct {
+	typ     byte   // '+', '-', ':', '$', '*'
+	str     string // simple string, error, bulkstring
+	integer int
+	array   []RespValue
+}
+
+type CommandHandler func(args []RespValue) string
+
+var commands = map[string]CommandHandler{
+	"PING": handlePing,
+	"ECHO": handleEcho,
+}
 
 func main() {
 
@@ -14,10 +29,10 @@ func main() {
 		os.Exit(1)
 	}
 	consumeListener(l)
-
 }
 
 func consumeListener(l net.Listener) {
+	defer l.Close()
 	for {
 		connection, err := l.Accept()
 
@@ -34,6 +49,7 @@ func consumeListener(l net.Listener) {
 }
 
 func handleConnection(connection net.Conn) {
+	defer connection.Close()
 	for {
 		buff := make([]byte, 1024)
 		_, err := connection.Read(buff)
@@ -41,7 +57,137 @@ func handleConnection(connection net.Conn) {
 			fmt.Println("Connection closed")
 			return
 		}
-		fmt.Println("Writing PONG response")
-		connection.Write([]byte("+PONG\r\n"))
+		val, _, err := RespParser(buff)
+		if err != nil {
+			fmt.Println("Parse error:", err)
+			connection.Write([]byte("-ERR parse error\r\n"))
+			continue
+		}
+		if val.typ != '*' || len(val.array) == 0 {
+			connection.Write([]byte("-ERR expected array\r\n"))
+			continue
+		}
+		cmd := strings.ToUpper(val.array[0].str)
+		args := val.array[1:]
+
+		handler, ok := commands[cmd]
+		if !ok {
+			connection.Write([]byte("-ERR unknown command\r\n"))
+			continue
+		}
+		connection.Write([]byte(handler(args)))
 	}
+}
+
+func handlePing(args []RespValue) string {
+	return "+PONG\r\n"
+}
+
+func handleEcho(args []RespValue) string {
+	if len(args) < 1 {
+		return "-ERR wrong number of arguments\r\n"
+	}
+	s := args[0].str
+	return fmt.Sprintf("$%d\r\n%s\r\n", len(s), s)
+}
+
+func RespParser(buff []byte) (RespValue, int, error) {
+	if len(buff) == 0 {
+		return RespValue{}, 0, fmt.Errorf("empty input")
+	}
+	typ := buff[0]
+	switch typ {
+	case '+', '-':
+		end := findCRLF(buff, 1)
+		if end == -1 {
+			return RespValue{}, 0, fmt.Errorf("missing CRLF")
+		}
+		return RespValue{typ: typ, str: string(buff[1:end])}, end + 2, nil
+
+	case ':':
+		end := findCRLF(buff, 1)
+		if end == -1 {
+			return RespValue{}, 0, fmt.Errorf("missing CRLF")
+		}
+		n, err := parseInteger(buff[1:end])
+		if err != nil {
+			return RespValue{}, 0, err
+		}
+		return RespValue{typ: typ, integer: n}, end + 2, nil
+
+	case '$': //bulk string
+		end := findCRLF(buff, 1)
+		if end == -1 {
+			return RespValue{}, 0, fmt.Errorf("missing CRLF")
+		}
+		length, err := parseInteger(buff[1:end])
+		if err != nil {
+			return RespValue{}, 0, err
+		}
+		if length == -1 {
+			return RespValue{typ: typ, str: ""}, end + 2, nil // null bulk string
+		}
+		start := end + 2
+		newEnd := int(start) + length
+		if int(len(buff)) < +2 {
+			return RespValue{}, 0, fmt.Errorf("incomplete bulk string")
+		}
+		return RespValue{typ: typ, str: string(buff[start:newEnd])}, int(newEnd + 2), nil
+
+	case '*': // array
+		end := findCRLF(buff, 1)
+		if end == -1 {
+			return RespValue{}, 0, fmt.Errorf("missing CRLF")
+		}
+		lenArr, err := parseInteger(buff[1:end])
+		if err != nil {
+			return RespValue{}, 0, err
+		}
+		pos := end + 2
+		elements := make([]RespValue, 0, lenArr)
+		for i := 0; i < lenArr; i++ {
+			val, consumed, err := RespParser(buff[pos:])
+			if err != nil {
+				return RespValue{}, 0, err
+			}
+			elements = append(elements, val)
+			pos += consumed
+		}
+		return RespValue{typ: typ, array: elements}, pos, nil
+
+	default:
+		return RespValue{}, 0, fmt.Errorf("unknown type byte: %c", typ)
+	}
+}
+
+func findCRLF(buff []byte, start int) int {
+	for i := start; i < len(buff)-1; i++ {
+		if buff[i] == '\r' && buff[i+1] == '\n' {
+			return i
+		}
+	}
+	return -1
+}
+
+func parseInteger(b []byte) (int, error) {
+	if len(b) == 0 {
+		return 0, fmt.Errorf("empty integer")
+	}
+	neg := false
+	start := 0
+	if b[0] == '-' {
+		neg = true
+		start = 1
+	}
+	var n int
+	for _, c := range b[start:] {
+		if c < '0' || c > '9' {
+			return 0, fmt.Errorf("invalid integer byte: %c", c)
+		}
+		n = n*10 + int(c-'0')
+	}
+	if neg {
+		n = -n
+	}
+	return n, nil
 }
